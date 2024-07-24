d6c7d3a5e1f91acb13948fce972fca75c2c74e03
Jordan <jordanlozano1123@gmail.com>
2024-07-24 14:21:29 -0500

    feat(BLL): Added method to search, update, delete and list the clients

---
+++ BLL/Exceptions/CustomerExceptions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Exceptions
+{
+    public class CustomerExceptions : Exception
+    {
+        private CustomerExceptions(string message) : base(message) 
+        { 
+
+        }
+
+        public static void ThrowCustomerAlreadyExistsException(string firstname, string lastname)
+        {
+            throw new CustomerExceptions($"A client with the name already exists {firstname} {lastname}.");
+        }
+
+        public static void ThrowInvalidCustomerDataException(string message)
+        {
+            throw new CustomerExceptions(message);
+        }
+
+        public static void ThrowInvalidCustomerIdException(int id)
+        {
+            throw new CustomerExceptions($"The client with id {id} was not found or dont exist");
+        }
+
+    }
+}
